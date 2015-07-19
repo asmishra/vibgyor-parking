3a88147553f317d16dcb238b201a19a6f08cc914
Anurag <anurago.asm@gmail.com>
2015-07-19 15:28:55 -0700

    Initial Commit

---
+++ vibgyor_mapview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  vibgyor_mapview
+//
+//  Created by Anurag Mishra on 7/19/15.
+//  Copyright (c) 2015 mojers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ vibgyor_mapviewTests/vibgyor_mapviewTests.m
@@ -0,0 +1,40 @@
+//
+//  vibgyor_mapviewTests.m
+//  vibgyor_mapviewTests
+//
+//  Created by Anurag Mishra on 7/19/15.
+//  Copyright (c) 2015 mojers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface vibgyor_mapviewTests : XCTestCase
+
+@end
+
+@implementation vibgyor_mapviewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
