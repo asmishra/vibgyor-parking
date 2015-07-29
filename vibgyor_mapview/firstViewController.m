1577f617f1a8c502e0c78463cb8010eb1b561f79
Anurag <anurago.asm@gmail.com>
2015-07-28 23:43:59 -0700

    Added sample usage of MKLocalSearchRequest. Annotating returned locations on mapview too.

--- vibgyor_mapview/firstViewController.m
+++ vibgyor_mapview/firstViewController.m
@@ -116,8 +116,50 @@
         NSLog(@"Parking location saved at %@", self.locationProperty.description);
         //Store this location as source location
         self.sourceLocation = self.locationProperty;
-        
         //Anurag - ASYNCH CALL HANDLING
+        
+        /******************************************/
+        //Add a tester for MKLocalSearchRequest
+        MKLocalSearchRequest *request = [[MKLocalSearchRequest alloc] init];
+        request.naturalLanguageQuery = @"indian restaurants"; //set NL query phrase
+        
+        MKCoordinateRegion region;
+        region.center.longitude = self.sourceLocation.coordinate.longitude;
+        region.center.latitude = self.sourceLocation.coordinate.latitude;
+        region.span.longitudeDelta = 0.1;
+        region.span.latitudeDelta = 0.1;
+        
+        request.region = region; //set region for the query
+        
+        MKLocalSearch *localSearch = [[MKLocalSearch alloc] initWithRequest:request];
+        [localSearch startWithCompletionHandler:^(MKLocalSearchResponse *response, NSError *error) {
+            if (!error) {
+                for (MKMapItem *mapItem in [response mapItems]) {
+                    NSLog(@"Name: %@, Placemark title: %@", [mapItem name], [[mapItem placemark] title]);
+                }
+            } else {
+                NSLog(@"Search Request Error: %@", [error localizedDescription]);
+            }
+        
+        //response has mapitems with information for our use
+        for (MKMapItem *mapItem in [response mapItems]) {
+            MKPointAnnotation *point = [[MKPointAnnotation alloc] init];
+            point.coordinate = self.locationProperty.coordinate; //The location received inside this function
+            NSMutableString *responseString = [[NSMutableString alloc] initWithFormat:@"%@", mapItem.name];
+            point.title = responseString;
+            NSLog(@"response string: %@ and coordinates (%f,%f)", point.title, point.coordinate.latitude, point.coordinate.longitude);
+            [self.mapView addAnnotation:point];
+            point = nil; //clean up the pointer?
+        }
+            self.mapView.userTrackingMode=YES;
+            [self.mapView setRegion:region animated:YES]; //change map to point to current location
+            [self.mapView regionThatFits:region];
+
+
+
+        }];
+        /******************************************/
+        
         
     }];
     
